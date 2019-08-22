9b2e315918c7c696f43205f3338643f994233b9e
edgarplazola <edgarplazola@gmail.com>
2019-08-21 20:52:58 -0500

    Agregando observadores y promesas

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -12,7 +12,9 @@ export class SidebarService {
       submenu: [
         { titulo: 'DashBoard', url: '/dashboard' },
         { titulo: 'ProgressBar', url: '/progress' },
-        { titulo: 'Gráficas', url: '/graficas1' }
+        { titulo: 'Gráficas', url: '/graficas1' },
+        { titulo: 'Promesas', url: '/promesas' },
+        { titulo: 'RXJS', url: '/rxjs' }
       ]
     }
   ];
